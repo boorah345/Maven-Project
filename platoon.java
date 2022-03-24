b0f0538905bbe26a7dc720ff92bcd6acd7e0e115
boorah345 <bura4precious@gmail.com>
2022-03-24 15:14:56 +0100

    hi

--- platoon.java
+++ platoon.java
@@ -5,6 +5,13 @@
     <groupId>org.apache.poi</groupId>
     <artifactId>poi</artifactId>
     <version>5.0.0</version>
+    <!-- https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml -->
+<dependency>
+    <groupId>org.apache.poi</groupId>
+    <artifactId>poi-ooxml</artifactId>
+    <version>5.0.0</version>
+</dependency>
+
 </dependency>
 
 </dependencies>

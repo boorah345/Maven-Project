b2edb52e8c16b207f2e4b34a801f532d399cd443
boorah345 <bura4precious@gmail.com>
2022-03-24 15:12:54 +0100

    dependency

---
+++ platoon.java
@@ -0,0 +1,11 @@
+<project>
+<dependencies>
+<!-- https://mvnrepository.com/artifact/org.apache.poi/poi -->
+<dependency>
+    <groupId>org.apache.poi</groupId>
+    <artifactId>poi</artifactId>
+    <version>5.0.0</version>
+</dependency>
+
+</dependencies>
+</project>
